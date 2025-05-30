f7692f007c8dc96561fb34629a4f698176e90d40
subhamoy-char <136074184+subhamoy-char@users.noreply.github.com>
2025-05-30 15:19:53 +0530

    Create VASP-PBS-submission.sh

---
+++ BASH-Script/VASP-PBS-submission.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#PBS -N test
+#PBS -q mini
+#PBS -l nodes=1:ppn=20
+#PBS -j oe
+#
+echo ""
+#Reading Settings and Variables
+source /home/subhamoy/.bashrc
+cd $PBS_O_WORKDIR
+ulimit -s unlimited
+export OMP_NUM_THREADS=1
+###
+cat $PBS_NODEFILE > pbs_nodes
+NPROCS=`wc -l < $PBS_NODEFILE`
+NNODES=`uniq $PBS_NODEFILE | wc -l`
+echo Running on host `hostname`
+echo Directory is `pwd`
+echo Using ${NPROCS} processors across ${NNODES} nodes
+###
+export VASP_BIN='/home/subhamoy/Software/VASP/bin/vaspEDD/vasp_std'
+echo Start time is `date`
+#
+echo "Job Started"
+#
+mpirun -np ${NPROCS} $VASP_BIN >STDOUT
+#
+echo End time is `date`
+#end
